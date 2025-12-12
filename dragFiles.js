c8506988eb2b675fa66f06ce094f707bbcbf89a2
freaky-hue <pip8836@gmail.com>
2025-12-12 15:29:11 +0000

    Add files via upload

--- dragFiles.js
+++ dragFiles.js
@@ -1,212 +1,279 @@
 "use strict";
 
+
 module.exports.dragFiles = function (parent) {
-    const obj = {};
-    obj.parent = parent;
-    obj.meshServer = parent.parent;
+  const obj = {};
+  obj.parent = parent;
+  obj.meshServer = parent.parent;
+  obj.VIEWS = __dirname + '/views/';
 
-    const path = require("path");
-    const fs = require("fs");
-    const utils = require("util");
-    const cp = require("child_process");
-    const os = require("os");
+  const path = require("path");
+  const fs = require("fs");
+  const utils = require("util");
+  const cp = require("child_process");
+  const fsPromises = require("fs/promises");
+  //const os = require("os");
+  let loginInfo = fs.existsSync(path.join(__dirname, "config", "config.json")) ? JSON.parse(fs.readFileSync(path.join(__dirname, "/config/config.json")).toString()) : 0;
 
-    const config = JSON.parse(
-        fs.readFileSync(path.join(__dirname, "config", "config.json"))
-    );
+  const exec = utils.promisify(cp.exec);
+  obj.dirFiles = path.join(__dirname, "../../../meshcentral-files", "domain");
 
-    const exec = utils.promisify(cp.exec);
-    obj.dirFiles = path.join(__dirname, "../../../meshcentral-files", "domain");
 
-    obj.mainFunc = async function (dir) {
-        const info = await new Promise((resolve, reject) => {
-            obj.meshServer.db.GetAll((err, info) => {
-                resolve(info);
-            
-            });
-        });
 
-        const agents = info.filter(
-            (td) => td.type == "node" && td.meshid == String(dir).replace("-", "//")
-        );
+  obj.server_startup = async function () {
+    obj.meshServer.webserver.app.route('/plugin/dragfiles/config')
+      .post((req, res) => {
+        let bodyInfo = ""
+        req.on("data", async (data) => {
+          bodyInfo += data.toString()
+        })
 
-        for (const agent of agents) {
-            await obj.checkMeshFilesEx(agent._id, dir);
-        }
-    };
+        req.on("end", async () => {
+          try {
+            loginInfo = JSON.parse(bodyInfo);
+            const testCred = await exec(`node .\\node_modules\\meshcentral\\meshctrl devicemessage --id --msg --loginuser ${loginInfo.user} --loginpass ${loginInfo.pass} `)
 
-    obj.checkMeshFilesEx = async function (agent_id, dir) {
-        const agentid = agent_id.split("//")[1];
-        const com = await exec(
-            `node node_modules/meshcentral/meshctrl runcommand --id "${agentid}" --run "powershell ls ${config.agent_root}" --reply --loginuser "${config.user}" --loginpass "${config.pass}" `
-        );
+            if (testCred.stdout.includes("Invalid")) {
+              res.status(400).send({ status: 'Error', message: "Error, invalid credentials" });
 
-        const stdout = com.stdout;
+            } else {
+              res.status(200).send({ status: 'OK', message: "File inserted with success" })
 
-        const stdoutSep = obj.parsePowerShellOutput(stdout);
-
-        const exD = stdoutSep.some(
-            (ex) => ex.type === "directory" && ex.name === "meshfiles"
-        );
-
-        if (!exD) {
-            await obj.makeFoldMeshFiles(agentid);
-        }
-
-        console.log("meshfiles lctd");
-
-        await obj.checkMeshFilesFiles(agentid, dir);
-    };
-
-    obj.checkMeshFilesFiles = async function (agentid, dir) {
-        try {
-            const com = await exec(
-                `node node_modules/meshcentral/meshctrl runcommand --id "${agentid}" --run "powershell ls ${config.host_upload_folder}" --reply --loginuser "${config.user}" --loginpass "${config.pass}" `
-            );
-            const res = obj.parsePowerShellOutput(com.stdout);
-
-            try {
-                const nPath = path.join(obj.dirFiles, dir);
-
-                try {
-                    const files = fs.readdirSync(nPath);
-
-                    for (const file of files) {
-                        const filePath = path.join(nPath, file);
-                        const fileStats = fs.statSync(filePath);
-
-                        const exFile = res.some(
-                            (fe) => fe.name === file && fe.size === fileStats.size
-                        );
-
-                        if (!exFile) {
-                            await obj.uploadFile(agentid, filePath);
-                        }
-                    }
-                } catch {
-                    console.log("VAZIO");
-                }
-            } catch (err) {
-                console.log(err);
-            }
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    obj.uploadFile = async function (agentid, filePath) {
-        const com = await exec(
-            `node node_modules/meshcentral/meshctrl upload --id "${agentid}" --file "${filePath}" --target "${config.host_upload_folder}" --loginuser "${config.user}" --loginpass "${config.pass}"`
-        );
-        com.stdout;
-    };
-
-    obj.makeFoldMeshFiles = async function (agentid) {
-        const com = await exec(
-            `node node_modules/meshcentral/meshctrl runcommand --id "${agentid}" --run "powershell mkdir ${config.host_upload_folder}" --loginuser "${config.user}" --loginpass "${config.pass}"`
-        );
-        await new Promise((r) => setTimeout(r, 1000));
-        com.stdout;
-    };
-
-    let lastTask = Promise.resolve();
-
-    obj.hook_agentCoreIsStable = function (nodeObj) {
-        if (!nodeObj) return;
-
-        lastTask = lastTask
-            .then(async () => {
-                await obj.checkAgentMeshFiles(nodeObj.nodeid, nodeObj.meshid);
-            })
-            .catch((err) => console.error(err));
-    };
-
-    obj.server_startup = async function () {
-        let delay = null;
-
-        try {
-            fs.watch(obj.dirFiles, (ev, dirr) => {
-                if (delay) {
-                    clearTimeout(delay);
-                }
-
-                delay = setTimeout(() => {
-                    obj.mainFunc(dirr);
-                }, 100);
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    obj.checkAgentMeshFiles = async function (agentid, groupid) {
-        try {
-            const com = await exec(
-                `node node_modules/meshcentral/meshctrl runcommand --id "${agentid}" --run "powershell ls ${config.agent_root}" --reply --loginuser "${config.user}" --loginpass "${config.pass}" `
-            );
-
-            const info = obj.parsePowerShellOutput(com.stdout);
-
-            const meshFExists = info.some(
-                (ex) => ex.type === "directory" && ex.name === "meshfiles"
-            );
-
-            console.log(meshFExists);
-
-            if (!meshFExists) {
-                await new Promise((r) => setTimeout(r, 2000));
-
-                await obj.makeAgentMeshFiles(agentid, groupid);
+              if (fs.existsSync(path.join(__dirname, "config"))) {
+                console.log("Exist")
+                await fsPromises.writeFile(path.join(__dirname, 'config', 'config.json'), JSON.stringify(loginInfo), { flag: 'w', encoding: 'utf8' })
+              } else {
+                await fsPromises.mkdir(path.join(__dirname, "config"))
+                  .then(async () => { await fsPromises.writeFile(path.join(__dirname, 'config', 'config.json'), JSON.stringify(loginInfo), { flag: 'w', encoding: 'utf8' }) })
+              }
             }
 
-            await obj.checkMeshFilesFiles(agentid, "mesh-" + groupid);
-        } catch (err) {
+            console.log(testCred.stdout);
+
+          } catch (err) {
             console.log(err);
+
+          }
+
+        })
+
+
+      })
+
+    await obj.mainFunc();
+
+
+  }
+
+
+  obj.mainFunc = async function () {
+    console.log("main func");
+
+  }
+
+
+  let runTask = Promise.resolve();
+
+  obj.hook_agentCoreIsStable = function (nodeObj) {
+    if (loginInfo != 0) {
+
+      if (!nodeObj) { return };
+
+      runTask = runTask.then(async () => {
+        await obj.onAgentConnect(nodeObj);
+
+      })
+    } else {
+      console.log("DragFiles -> Plugin config.json file is not created");
+
+    }
+  };
+
+  obj.onAgentConnect = async function (agentObject) {
+
+    const objc = {
+      id: agentObject.nodeid,
+      groupfolder: agentObject.dbMeshKey.replace("//", "-"),
+      name: agentObject.name
+    };
+
+    await obj.onAgentConnectCheckMeshFiles(objc.id, objc.groupfolder, objc.name);
+  }
+
+  obj.onAgentConnectCheckMeshFiles = async function (agentid, groupfold, agentname) {
+    try {
+      const checkF = await exec(`node ./node_modules/meshcentral/meshctrl runcommand --id "${agentid}" --run "dir ${loginInfo.host_upload_folder.split("/")[0]}\\ " --loginuser "${loginInfo.user}" --loginpass "${loginInfo.pass}" --reply`);
+
+      const rep = obj.convertToJson(checkF.stdout);
+      const foldEx = rep.find((fil) => fil.Name == "meshfiles");
+
+      if (foldEx == undefined) {
+        await obj.mkMeshFiles(agentid);
+      }
+
+      await obj.checkMfFiles(agentid, groupfold, agentname);
+    } catch (err) {
+      console.error("dragFiles -> Err folder meshfiles:", err);
+    }
+  }
+
+  obj.mkMeshFiles = async function (agentid) {
+    try {
+      await exec(`node ./node_modules/meshcentral/meshctrl runcommand --id "${agentid}" --run "mkdir ${loginInfo.host_upload_folder.split("/")[0]}\\meshfiles " --loginuser "${loginInfo.user}" --loginpass "${loginInfo.pass}" --reply`);
+      console.log("DragFiles -> MeshFiles has been created");
+    } catch (err) {
+      console.error("dragFiles -> Err (creating) folder meshfiles:", err);
+    }
+  }
+
+  obj.checkMfFiles = async function (agentid, groupfold, agentname) {
+    try {
+      const checkMF = await exec(`node ./node_modules/meshcentral/meshctrl runcommand --id "${agentid}" --run "dir ${loginInfo.host_upload_folder.split("/")[0]}\\meshfiles\\ " --loginuser "${loginInfo.user}" --loginpass "${loginInfo.pass}" --reply`);
+
+      const rep = obj.convertToJson(checkMF.stdout);
+      await obj.connectUploadFile(agentid, groupfold, rep, agentname);
+    } catch (err) {
+      console.error("dragFiles -> Err files:", err);
+    }
+  }
+
+  obj.connectUploadFile = async function (agentid, foldername, agentfiles, agentname) {
+    try {
+      const groupPath = path.join(obj.dirFiles, foldername);
+
+      if (!fs.existsSync(groupPath)) {
+        console.log(`dragFiles -> Group fold doesnt exist: ${foldername}`);
+        return;
+      }
+
+      const files = await fs.promises.readdir(groupPath);
+
+      for (const file of files) {
+        const filePath = path.join(groupPath, file);
+
+        const stats = await fs.promises.stat(filePath);
+
+
+        const ex = agentfiles.some((wtf) => { return wtf['Size_Bytes'] == stats.size && wtf['Name'] == file });
+
+
+
+        if (stats.isFile()) {
+          if (!ex) {
+            await obj.upFiles(filePath, agentid, agentfiles, file);
+          }
+
+        } else if (stats.isDirectory() && file == agentname) {
+          console.log(agentid);
+
+          await obj.openAgentNameFolder(agentid, foldername, filePath, agentname, agentfiles);
+
         }
-    };
+      }
 
-    obj.makeAgentMeshFiles = async function (agentid, groupid) {
-        try {
-            if (fs.existsSync(path.join(obj.dirFiles, "mesh-" + groupid))) {
-                const com = await exec(
-                    `node node_modules/meshcentral/meshctrl runcommand --id "${agentid}" --run "powershell mkdir ${config.host_upload_folder}" --reply --loginuser "${config.user}" --loginpass "${config.pass}" `
-                );
-                await new Promise((r) => setTimeout(r, 1000));
+      console.log(`dragFiles -> Upload has ended${agentid}`);
+    } catch (err) {
+      console.error(`dragFiles -> Err uploads`, err);
+    }
+  }
 
-                com.stdout;
-            }
-        } catch (err) {
-            console.log(err);
+  obj.openAgentNameFolder = async function (agentid, foldername, filePath, agentname, af) {
+
+    try {
+      const fls = await fs.promises.readdir(path.join(filePath));
+
+      if (fls) {
+
+        for (const file of fls) {
+          const stats = await fs.promises.stat(path.join(filePath, file))
+
+
+
+          const ex = af.some((wtf) => { return wtf['Size_Bytes'] == stats.size && wtf['Name'] == file });
+
+
+          // const ex = af.hasOwn()((fl) =>  fl["Size_Bytes"] == stats.size && fl["Name"] == file );
+
+
+          if (!ex) {
+            await obj.upFiles(path.join(filePath, file), agentid, af, file)
+
+          }
+
+          // if (!ex) {
+
+          // }
+
+
         }
-    };
+      }
 
-    obj.parsePowerShellOutput = function (output) {
-        return output
-            .split("\n")
-            .map((line) => line.trim())
-            .filter(
-                (line) =>
-                    line &&
-                    !line.startsWith("Mode") &&
-                    !line.startsWith("----") &&
-                    !line.startsWith("Directory") &&
-                    !line.startsWith("Microsoft Windows") &&
-                    !line.startsWith("(c)") &&
-                    !line.includes(">") &&
-                    !line.includes("exit")
-            )
-            .map((line) => {
-                const parts = line.split(/\s+/);
-                const typeChar = parts[0]?.[0] ?? "f";
-                return {
-                    type: typeChar === "d" ? "directory" : "file",
-                    date: parts[1],
-                    time: parts[2],
-                    size: typeChar === "d" ? null : parseInt(parts[3], 10),
-                    name: parts.slice(typeChar === "d" ? 3 : 4).join(" "),
-                };
-            });
-    };
 
-    return obj;
+
+    } catch (error) {
+      console.log(`dragFiles -> Err agent name folder ${error} : ${agentid}`);
+
+    }
+
+  }
+
+  obj.upFiles = async function (fileP, agentid, af, file) {
+    try {
+      console.log(`dragFiles -> Waiting ${file}...`);
+
+      await exec(`node ./node_modules/meshcentral/meshctrl upload --id "${agentid}" --file "${fileP}" --target "C:\\meshfiles" --loginuser "${loginInfo.user}" --loginpass "${loginInfo.pass}"`);
+
+      console.log(`dragFiles -> Uploaded ${file}`);
+    } catch (err) {
+      console.error(`dragFiles -> Err upload ${file}:`, err.message);
+    }
+  }
+
+
+
+
+
+  obj.handleAdminReq = function (req, res, user) {
+
+    if (!user || user.siteadmin !== 0xFFFFFFFF) {
+      res.status(403).send("dragFiles-> Denied");
+      return
+    }
+
+    res.render(obj.VIEWS + "checkConfig.handlebars", { title: "Drag Files", fileEx: fs.existsSync(path.join(__dirname, "config", "config.json")) })
+  }
+
+
+  obj.hook_webServer = function (req, res, next) {
+    if (req.path === "/dragFiles/") {
+      obj.handleAdminReq(req, res, req.session.user);
+      return;
+    }
+    next();
+  }
+
+  obj.convertToJson = function (info) {
+    const itemRegex = /(\d{2}[\/\-]\d{2}[\/\-]\d{4})\s+(\d{2}:\d{2}(?:\s*[AP]M)?)\s+((?:<DIR>)|(?:[\d\.,ÿ\s]+))\s+(.+)/i;
+
+    return info.split(/\r?\n/)
+      .map(line => {
+        const match = line.trim().match(itemRegex);
+        if (match) {
+          const typeOrSize = match[3].trim();
+          const isDirectory = typeOrSize.includes('DIR');
+
+          return {
+            Name: match[4].trim(),
+            Type: isDirectory ? 'Directory' : 'File',
+            Size_Bytes: isDirectory ? null : parseInt(typeOrSize.replace(/[^\d]/g, ''), 10),
+            Last_Modified_Date: match[1],
+            Last_Modified_Time: match[2]
+          };
+        }
+        return null;
+      })
+      .filter(item => item !== null);
+  }
+
+  return obj;
 };
